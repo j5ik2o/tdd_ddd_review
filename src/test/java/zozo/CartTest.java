1aa1cae10e6d3d54c65a802a9c633f479c406f93
Junichi Kato <j5ik2o@gmail.com>
2022-02-08 15:12:44 +0900

    Update TODO&Test

--- src/test/java/zozo/CartTest.java
+++ src/test/java/zozo/CartTest.java
@@ -28,17 +28,27 @@ public class CartTest {
         }
     }
 
-    @Test
-    public void カートから追加された商品を削除する() {
+    @Nested
+    public class カートから追加された商品を削除する {
+        @Test
+        public void カートから追加された商品1を削除する() {
 
+        }
     }
 
-    @Test
-    public void カートから追加された商品の数量を変更する() {
+    @Nested
+    public class カートから追加された商品の数量を変更する {
+        @Test
+        public void カートから追加された商品1の数量1から2を変更する() {
 
+        }
     }
+    @Nested
+    public class カートの内容を確認する {
 
-    @Test
-    public void カートの内容を確認する() {
+        @Test
+        public void 商品1を1個追加したカートの内容を確認する() {
+        }
+
     }
 }
